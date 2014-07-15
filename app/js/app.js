19a6773c44ad4ab5e95604c4daba276d42abe18e
Nicki <Nicki@192.168.1.70>
2014-07-15 23:10:46 +0200

    initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,15 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('gyrateCustomer', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'gyrateCustomer.filters',
+  'gyrateCustomer.services',
+  'gyrateCustomer.directives',
+  'gyrateCustomer.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/create', {templateUrl: 'partials/createCustomer.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/search', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
+angular.module('gyrateCustomer.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
 
   }])
