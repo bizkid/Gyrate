667702a50f8ef49878ebc470964b751eb2cf188c
Nicki <Nicki@192.168.1.70>
2014-07-19 22:58:50 +0200

    saving progress

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,11 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('gyrateCustomer.services', []).
-  value('version', '0.1');
+var customerServices = angular.module('gyrateCustomer.services', [ngResource]);
+customerServices.factory('Customer', ['$resource', 
+    function($resource){
+        return $resource('/customer', {}, {
+            query: {method: 'GET', params:{customerId:'customers'}, isArray:true},
+            save: {method: 'POST'}
+        });
+    }]);
